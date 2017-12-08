cc1f3c65be7d79a05ea8596ef9c45a9ce72f0398
Hoon Lee <yl.hoony@gmail.com>
2017-12-08 17:54:01 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,11 +7,13 @@ class Anagram
   end
 
   def match(random_words)
-    result = []
-    random_words.each do |random_word|
-      result << random_word if random_word.chars.sort.join == self.word.chars.sort.join
-    end
-    result
+    # result = []
+    # random_words.each do |random_word|
+    #   result << random_word if random_word.chars.sort.join == self.word.chars.sort.join
+    # end
+    # result
+
+    random_words.select { |random_word| random_word.chars.sort.join == self.word.chars.sort.join }
   end
 
 end
